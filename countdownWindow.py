084606495370ead0e53c5e63ea89897e015be38a
hnewey7 <hnewey7@gmail.com>
2023-07-21 23:43:53 +0100

    Editing countdown window
    Editing countdown window with correct basic formatting.

--- countdownWindow.py
+++ countdownWindow.py
@@ -29,15 +29,15 @@ def countdownWindow(applicationName):
     # Creating window with tkinter.
     countdownWindow = tk.Tk()
     countdownWindow.title(applicationName)
-    countdownWindow.geometry("700x300")
+    countdownWindow.geometry("700x150")
     
     # Creating title for countdown window.
-    title = tk.Label(countdownWindow, text=applicationName)
+    title = tk.Label(countdownWindow, text=applicationName, pady=5)
     
     # Creating buttons.
-    addButton = tk.Button(countdownWindow, text="Add", command=addEvent)
-    editButton = tk.Button(countdownWindow, text="Edit", command=editEvent)
-    deleteButton = tk.Button(countdownWindow, text="Delete", command=deleteEvent)
+    addButton = tk.Button(countdownWindow, text="Add", command=addEvent, width=15)
+    editButton = tk.Button(countdownWindow, text="Edit", command=editEvent, width=15)
+    deleteButton = tk.Button(countdownWindow, text="Delete", command=deleteEvent, width=15)
     
     # Formatting title.
     title.grid(row=1,column=1,columnspan=3)
@@ -46,6 +46,19 @@ def countdownWindow(applicationName):
     addButton.grid(row=2,column=1)
     editButton.grid(row=2,column=2)
     deleteButton.grid(row=2,column=3)
+    
+    # Configuring rows to align vertically.
+    countdownWindow.grid_rowconfigure(0, weight=1)
+    countdownWindow.grid_rowconfigure(1, weight=1)
+    countdownWindow.grid_rowconfigure(2, weight=1)
+    countdownWindow.grid_rowconfigure(3, weight=1)
+    
+    # Configuring columns to align horizontally.
+    countdownWindow.grid_columnconfigure(0, weight=1)
+    countdownWindow.grid_columnconfigure(1, weight=1)
+    countdownWindow.grid_columnconfigure(2, weight=1)
+    countdownWindow.grid_columnconfigure(3, weight=1)
+    countdownWindow.grid_columnconfigure(4, weight=1)
     
     # Lifting window 
     countdownWindow.lift()
