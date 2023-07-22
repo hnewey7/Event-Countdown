c23ace23fabb4f5c14c1eaffb41f3a795eccd1c0
hnewey7 <hnewey7@gmail.com>
2023-07-22 17:32:06 +0100

    Update addWindow.py
    Added setName() which sets the name and clears the input box.

--- addWindow.py
+++ addWindow.py
@@ -14,6 +14,7 @@ import datetime
 # Function to display add event window.
 def addWindow():
     
+    # Add event function
     def addButton():
         
         # Get current date.
@@ -22,21 +23,35 @@ def addWindow():
         # Get date from calendar.
         date = calendar.selection_get()
         
+        # If date is in the future.
         if date > currentDate:
             
             return()
         
         else:
             
+            # Error message popup.
             messagebox.showerror("Previous Date", "Selected event date is in the past. Please select a date in the future.")
-  
+           
+    # Set event name function.
+    def setName():
+        
+        # Get data from entry.
+        name = eventName.get()
+        
+        # Clear entry
+        eventName.delete(0,tk.END)
+        
+        return()
+        
     # Creating window with tkinter.
     addWindow = tk.Tk()
     addWindow.title("Add Event")
     addWindow.geometry("600x220")
     
     # Defining variables.
-    eventSummary = "Event"
+    eventSummary = ""
+    name = ""
     addHours = tk.IntVar()
     addMins = tk.IntVar()
     
@@ -48,12 +63,12 @@ def addWindow():
     
     # Creating event name elements.
     eventName = tk.Entry(addWindow, width=19)
-    setName = tk.Button(addWindow, text="Set Name", width=8)
+    setNameButton = tk.Button(addWindow, text="Set Name", width=8, command=setName)
     
     # Creating time entry elements.
     addHoursTime = ttk.Spinbox(addWindow, from_=0, to=23, textvariable=addHours, width=6)
     addMinsTime = ttk.Spinbox(addWindow, from_=0, to=59, textvariable=addMins, width=6)
-    setTime = tk.Button(addWindow, text="Set Time", width=8)
+    setTimeButton = tk.Button(addWindow, text="Set Time", width=8)
     
     # Creating add event button.
     addEvent = tk.Button(addWindow, text="Add", width=15, command=addButton)
@@ -66,12 +81,12 @@ def addWindow():
     
     # Formatting event name elements.
     eventName.grid(row=2, column=2, columnspan=2)
-    setName.grid(row=2, column=4, sticky="w")
+    setNameButton.grid(row=2, column=4, sticky="w")
     
     # Formatting time entry elements.
     addHoursTime.grid(row=3, column=2, sticky='ne', padx=5)
     addMinsTime.grid(row=3, column=3, sticky='nw', padx=5)
-    setTime.grid(row=3, column=4, sticky='nw')
+    setTimeButton.grid(row=3, column=4, sticky='nw')
     
     # Formatting add event button.
     addEvent.grid(row=4, column=2, columnspan=3, sticky='n')
