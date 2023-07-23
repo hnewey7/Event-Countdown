96772edbe452af702caa47b1475d54fab1e4ba25
hnewey7 <hnewey7@gmail.com>
2023-07-23 20:18:00 +0100

    Adding init.py
    Adding new script to define the init() function to define various variables and perform tasks on initialisation.

---
+++ init.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 23 20:16:03 2023
+
+@author: hnewey7
+"""
+
+# Function for initialisation.
+def init():
+    
+    # Defining global variables.
+    global applicationName
+    global geometry
+    
+    # Defining key values.
+    applicationName = "Event Countdown"
+    geometry = "700x150"
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Created on Fri Jul 21 23:24:23 2023
 @author: hnewey7
 """
 
+from init import init
 from countdownWindow import countdownWindow
 
 # Defining key values.
@@ -14,6 +15,7 @@ geometry = "700x150"
 # Main function.
 def main():
     
+    init()
     countdownWindow(applicationName, geometry)
         
 main()
