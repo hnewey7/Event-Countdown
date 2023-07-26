74a724f427c314ef1f55eb98f9ca59536d10765c
hnewey7 <hnewey7@gmail.com>
2023-07-26 21:07:41 +0100

    Removing init.py script
    Removing initialisation script and defining the function within the main script.

--- main.py
+++ main.py
@@ -5,16 +5,47 @@ Created on Fri Jul 21 23:24:23 2023
 @author: hnewey7
 """
 
-from init import init
+import os
 from countdownWindow import countdownWindow
 
+# Initialisation function
+def init():
+        
+    # Defining key values.
+    applicationName = "Event Countdown"
+    geometry = "700x150"
+    
+    # Getting events log path.
+    folderPath = os.path.dirname(os.path.abspath(__file__))
+    logPath = folderPath + "\\logs\\events.txt"
+    
+    # Check if file exists.
+    if os.path.exists(logPath):
+        
+        # Read file.
+        log = open(logPath,"r")
+        events = log.readlines()
+
+    else:
+        
+        # Create log path.
+        log = open(logPath,"x")
+    
+    return events, applicationName, geometry
+    
 # Main function.
 def main():
-    
-    # Initialisation function.
-    events, applicationName, geometry = init()
 
     # Main GUI function.
     countdownWindow(events, applicationName, geometry)
-        
+    
+def onClose():
+    
+    return
+     
+    
+# Initialising.
+events, applicationName, geometry = init()
+
+# Running main function.
 main()
